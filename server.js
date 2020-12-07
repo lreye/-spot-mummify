6cb8dd2c7211d6993da4b9544e537eeefb4dd042
Luis Reyes <l.reyes2224@gmail.com>
2020-12-06 20:49:30 -0600

    Added routes for login in and for authentication. The accesstoken is also save in DS db.

--- server.js
+++ server.js
@@ -12,15 +12,47 @@ const db = new Datastore();
 const PORT = 5000;
 const HOST = '0.0.0.0';
 const app = express();
+const clientURL = process.env.CLIENT_URL;
 
+
+//midleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
+//pusher
+const Pusher = require('pusher');
 
 //routes
+
+//authariztion route
+const authSpotify = require("./src/backend/authForSpotify");
+const getAccessToken = require('./src/backend/getAccessToken');
+app.get('/devlogin', authSpotify);
+app.get('/auth/welcome', getAccessToken);
+
+
+/*
+authorized user routes | must also create a file and follow 
+authariztion route as an example for future implementation
+*/
+app.get('/auth/welcome', getAccessToken, (req, res, next) => {
+    console.log("Hello Authorized User");
+    db.insert(req.credentials, err =>{
+        if(err){
+            next(err);
+        }
+        else{
+            console.log("Redirect After Saving Token");
+            res.redirect(`${clientURL}/?authorized=true`);
+        }
+    })
+});
+
+//home route
 app.get('/', (req, res) => {
-    res.send('Hello Welcome to Spotify Mummify');
+    console.log("Hello Welcome");
+    res.send('Hello Welcome');
 });
 
 app.set('port', PORT)
