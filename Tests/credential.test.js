bd9e9352f4c80214e90b87e68b53c2b2490d7571
Luis Reyes <l.reyes2224@gmail.com>
2020-12-08 22:49:33 -0600

    added a unit test for credentials module

---
+++ Tests/credential.test.js
@@ -0,0 +1,7 @@
+const credentials = require('../src/backend/credentials');
+
+test("Testing Credentials Module", () =>{
+    expect(credentials.client_id).toBe(process.env.CLIENT_ID);
+    expect(credentials.client_secret).toBe(process.env.CLIENT_SECRET);
+    expect(credentials.redirect_uri).toBe(process.env.REDIRECTURI);
+});
