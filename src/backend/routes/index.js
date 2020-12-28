a0990c81991df00eac13cfb026a85a67ecd073f1
Luis Reyes <l.reyes2224@gmail.com>
2020-12-27 22:26:08 -0600

    history route added

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const login = require('./login');
 const rootRoute = require('./root');
 const auth = require('./auth');
+const history = require('./history');
 
 const getRoutes = () => {
     const router = express.Router();
     router.use('/', rootRoute.getRootRoutes());
     router.use('/login', login.getLoginRoutes());
     router.use('/auth', auth.getAuthRoutes());
+    router.use('/history', history.getHistoryRoute());
     return router;
 };
 
